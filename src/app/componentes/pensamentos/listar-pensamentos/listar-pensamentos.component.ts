db3cd240ed2bf082585f1304d20b6eec99e803b6
Alisson <alissondvorak@gmail.com>
2023-02-08 09:52:56 -0300

    Projeto completo

---
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -0,0 +1,23 @@
+import { PensamentoService } from './../pensamento.service';
+import { Pensamento } from './../pensamento';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-pensamentos',
+  templateUrl: './listar-pensamentos.component.html',
+  styleUrls: ['./listar-pensamentos.component.css']
+})
+export class ListarPensamentosComponent implements OnInit {
+
+  listaPensamentos: Pensamento[] = [];
+
+  constructor(private service: PensamentoService) { }
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((ListaPensamentos) => {
+      this.listaPensamentos = ListaPensamentos
+    })
+  }
+}
+
+
